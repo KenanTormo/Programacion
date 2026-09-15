829488d30b53302e964fe2649310161b913d59de
KenanTormo <KenanTormo@alu.edu.gva.es>
2026-09-15 12:20:27 +0200

    Fecha

---
+++ 00_Intro/01_variable2.py
@@ -0,0 +1,15 @@
+
+print("Introduce tu año de nacimiento: ")
+cadena_nacimiento = input()
+nacimiento = int(cadena_nacimiento)
+
+
+print("Que edad cumples este año?")
+cadena_años = input()
+años = int(cadena_años)
+
+
+año_actual = nacimiento + años
+
+
+print("Estamos en el año: ", año_actual)
